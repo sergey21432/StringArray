c12a06e60db3160072a00b14179b8b2c7feae622
Serguest07 <sergey21432@yandex.ru>
2022-07-19 14:27:11 +0300

    Created cicle in main function

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
-﻿string[] selectionStringLength (string[] inputArray)
+﻿string[] SelectionStringLength(string[] inputArray)
 {
+    int count = 0;
     string[] outputArray = new string[0];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+            Array.Resize(ref outputArray, count);
+            outputArray[count - 1] = inputArray[i];
+        }
+    }
     return outputArray;
 }
